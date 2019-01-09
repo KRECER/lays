19c08864d0b6406ee5c12016a99f51f6afaa0d24
Nastya <a.derimarko@itcg.ua>
2019-01-09 18:52:47 +0200

    btn animation

--- src/js/plugins/scroll.js
+++ src/js/plugins/scroll.js
@@ -4,12 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var rePrizes = new RegExp('^/prizes/?$');
   if (rePrizes.test(window.location.pathname)) {
-    document.querySelector('.prizes').classList.add('show')
+    document.querySelector('.prizes').classList.add('show');
   }
 
   var reHollywood = new RegExp('^/hollywood/?$');
   if (reHollywood.test(window.location.pathname)) {
-    document.querySelector('.hollywood').classList.add('show')
+    document.querySelector('.hollywood').classList.add('show');
+    document.querySelector('.prizes').classList.add('show');
   }
 
   var reMain = new RegExp('^/main/?(\\?.+)?$');
@@ -18,36 +19,41 @@ document.addEventListener("DOMContentLoaded", function() {
   // Slider
   document.addEventListener('wheel', scrollDirection);
 
+  var isScrolling = false;
   function scrollDirection(e) {
-    if (document.querySelectorAll('.modal-wrapper.expanded').length === 0) {
-      if ((e.deltaY > 0)) {
-        if (document.getElementById('bottom-block') && (reMain.test(window.location.pathname) || reRoot.test(window.location.pathname)) ) {
-          console.log("render prizes");
-          window.history.pushState("", "", '/prizes');
-          document.getElementById('bottom-block').classList.add('show');
-        } else if (document.getElementById('js-scroll-hollywood') && rePrizes.test(window.location.pathname)) {
-          console.log("render hollywood");
-          window.history.pushState("", "", '/hollywood');
-          document.getElementById('js-scroll-hollywood').classList.add('show');
-        }
-      } 
-      else if (e.deltaY <= -100) {
-        if (document.getElementById('bottom-block') && reHollywood.test(window.location.pathname)) {
-          window.history.pushState("", "", '/prizes');
-          document.getElementById('js-scroll-hollywood').classList.remove('show');
-        }
-        else if (rePrizes.test(window.location.pathname)) {
-          window.history.pushState("", "", '/main');
-          document.getElementById('bottom-block').classList.remove('show');
-        }
+    console.log(e);
+    if (isScrolling) {
+      return;
+    } else {
+      isScrolling = true;
+    }
+    if ((e.deltaY > 0)) {
+      if (document.getElementById('bottom-block') && (reMain.test(window.location.pathname) || reRoot.test(window.location.pathname)) ) {
+        window.history.pushState("", "", '/prizes');
+        document.getElementById('bottom-block').classList.add('show');
+      } else if (document.getElementById('js-scroll-hollywood') && rePrizes.test(window.location.pathname)) {
+        window.history.pushState("", "", '/hollywood');
+        document.getElementById('js-scroll-hollywood').classList.add('show');
+      }
+    } else {
+      if (document.getElementById('bottom-block') && reHollywood.test(window.location.pathname)) {
+        window.history.pushState("", "", '/prizes');
+        document.getElementById('js-scroll-hollywood').classList.remove('show');
+      }
+      else if (rePrizes.test(window.location.pathname)) {
+        window.history.pushState("", "", '/main');
+        document.getElementById('bottom-block').classList.remove('show');
       }
     }
+
+    isScrolling = false;
   }
 
   //btn main
   var btnAnimation = document.getElementById('js-btn-animation');
   btnAnimation.addEventListener('click', function (event){
     this.classList.add('btn-animation');
+    document.getElementsByName('code')[0].focus();
   })
 
 });  
