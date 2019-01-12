49c211d93e8aba7438f7a3e5bcaacf36a1712e15
Artem <houdiini@gmail.com>
2019-01-12 18:47:58 +0200

    slider instructions

--- src/js/plugins/wslider.js
+++ src/js/plugins/wslider.js
@@ -1,17 +1,26 @@
 var tabletMedia = window.matchMedia('(max-aspect-ratio:3/3)');
 var slider = undefined;
+// var arrows = document.querySelectorAll('.js-arrow');
 
 function sliderInit() {
-  var swiper = new Swiper('.instruction', {
+  slider = new Swiper('.instruction', {
     slidesPerView: 1,
     wrapperClass: 'slideshow-container',
+    slideClass: 'instruction__item',
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+    },
   });
+  console.log(slider);
 }
+
+// arrows
 
 function sliderToggler(e) {
   console.log('tabletMedia', tabletMedia);
   if (tabletMedia.matches) {
-    !slider && sliderInit();
+    sliderInit();
   } else {
     slider && slider.destroy();
   }
