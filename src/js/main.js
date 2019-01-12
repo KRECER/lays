ad906493114737fa53effdf898698ce05cc8d482
Artem <houdiini@gmail.com>
2019-01-12 21:24:42 +0200

    close menu when ligin is open

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,23 @@ var modal = document.getElementsByClassName('enterform')[0];
 var form = document.getElementsByClassName('enterform__wrapper')[0];
 var closeSign = document.getElementsByClassName('enterform__close')[0];
 var headerBg = document.getElementsByClassName('page-header')[0];
-function signIn(){
+
+function signIn() {
 	modal.style.display = 'block';
 	modal.style.zIndex = '10';
-	closeSign.style.zIndex = 
-	'10';
+	closeSign.style.zIndex = '10';
 	headerBg.style.zIndex = '9';
+
+	document.getElementById('menu-block').classList.remove('expanded');
+	document.getElementById('menu-inner-block').classList.remove('expanded');
+	document.getElementById('close-menu-bg').classList.remove('expanded');
 }
-function hideModalCloseButton(){
+
+function hideModalCloseButton() {
 	modal.style.display = 'none';
 	headerBg.style.zIndex = '1';
 }
+
 function hideModalOverlay() {
 	modal.style.display = 'none';
 	headerBg.style.zIndex = '1';
@@ -30,18 +36,20 @@ var formReg = document.getElementsByClassName('registration__wrapper')[0];
 var closeSignReg = document.getElementsByClassName('registration__close')[0];
 var headerBg = document.getElementsByClassName('page-header')[0];
 var registrationBtn = document.getElementById('registrationBtn');
+
 function getRegistration() {
 	modalReg.style.display = 'block';
 	modalReg.style.zIndex = '10';
 	closeSignReg.style.zIndex = '10';
 	headerBg.style.zIndex = '9';
 	modal.style.display='none';
-
 }
+
 function hideModalRegistrationCloseButton() {
 	modalReg.style.display = 'none';
 	headerBg.style.zIndex = '1';
 }
+
 function hideModalRegistrationOverlay() {
 	modalReg.style.display = 'none';
 	headerBg.style.zIndex = '1';
