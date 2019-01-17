cfeaf5f34cfa88ea298799cccbfd5417cf7f5422
Bogdan <b.sinchuk@itcg.ua>
2019-01-17 21:19:51 +0200

    recaptcha: async defer; chat: on body load

--- src/js/main.js
+++ src/js/main.js
@@ -101,6 +101,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
 window.addEventListener('load', function() {
   indexPreloader.style.display = 'none';
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://widget.sender.mobi/build/init.js";
+    fjs.parentNode.insertBefore(js, fjs, 'sender-widget');
+  })(document, 'script');
 });
 
 
